f6a0f561c7acd0159192d68d60b1519e2057a32e
Neha Deshpande <neha.h.deshpande@gmail.com>
2018-02-12 10:32:04 -0500

    Add files via upload

--- medicare/laboratory/models.py
+++ medicare/laboratory/models.py
@@ -1,13 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class Patient(models.Model):
-    patient_id = models.CharField(max_length=5,primary_key=True)
-    patient_name=models.CharField(max_length=20)
-
-class Doctor(models.Model):
-    doctor_id = models.CharField(max_length=5,primary_key=True)
-    doctor_name = models.CharField(max_length=20)
 
 class Laboratory(models.Model):
     test_id = models.CharField(max_length=5)
