2cea0fce355c448b487bd0f45c4f4af0d5e152a3
trupa7 <35124558+trupa7@users.noreply.github.com>
2018-02-14 12:35:09 -0500

    Add files via upload

--- medicare/pharmacist/admin.py
+++ medicare/pharmacist/admin.py
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 from django.contrib import admin
 from .models import Pharmacist,Prescription
 # Register your models here.
 admin.site.register([Prescription,Pharmacist])
-=======
-from django.contrib import admin
-from .models import Pharmacist,Prescription
-# Register your models here.
-admin.site.register([Prescription,Pharmacist])
->>>>>>> bf50983dee3fcf5b70e95b4bb430d7795e532408
