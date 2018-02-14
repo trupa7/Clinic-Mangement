bb738da77b59b423df639cc5ba7e14266c2b1823
trupa7 <35124558+trupa7@users.noreply.github.com>
2018-02-14 12:33:09 -0500

    Add files via upload

--- medicare/login/urls.py
+++ medicare/login/urls.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 '''
 Created on Feb 11, 2018
 
@@ -25,31 +24,3 @@ urlpatterns = [
     url(r'^auth/$', views.auth_view, name='auth'),
 ]
 
-=======
-'''
-Created on Feb 11, 2018
-
-@author: trupa7
-'''
-'''
-Created on Feb 1, 2018
-
-@author: trupa7
-'''
-from django.urls import path
-from django.conf.urls import url
-from django.contrib import admin
-from django.contrib.auth import views as auth_views
-from django.conf.urls.static import static
-from django.views.generic.base import TemplateView
-from django.conf import settings
-from . import views
-app_name="login"
-urlpatterns = [
-    path('', views.index, name='index'),
-    url(r'^login/$', views.login, name='login'),
-    url(r'^logout/$', views.logout, name='logout'),
-    url(r'^auth/$', views.auth_view, name='auth'),
-]
-
->>>>>>> bf50983dee3fcf5b70e95b4bb430d7795e532408
